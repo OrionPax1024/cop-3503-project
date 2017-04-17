7ce89e3c5d141c718ef61fb63ec55adc8db1b44c
Brendan Cohen <brendanjcohen@icloud.com>
2017-04-16 22:05:22 -0400

    addStrength/Stealth range now 1-10 instead of 0-10

--- entity.h
+++ entity.h
@@ -95,13 +95,13 @@ void Player::printStats(int race) {
 }
 
 void Player::addStrength(){
-    int i = rand() % 10;
+    int i = (rand() % 9) + 1;
     cout << "You gained " << i << " Strength points\n" << "\n";
     Player::addStrength(i);
 }
 
 void Player::addStealth(){
-    int i = rand() % 10;
+    int i = (rand() % 9) + 1;
     cout << "You gained " << i << " Stealth points\n" << "\n";
     Player::addStealth(i);
 }
